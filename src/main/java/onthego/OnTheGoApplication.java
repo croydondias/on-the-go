cc3f6bd8497723dc18fd4f2a0d4899d1ff4b3773
Croydon Dias <croydondias@gmail.com>
2015-09-06 11:10:25 +0100

    Initial commit with project structure

---
+++ src/main/java/onthego/OnTheGoApplication.java
@@ -0,0 +1,12 @@
+package onthego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnTheGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnTheGoApplication.class, args);
+    }
+}
